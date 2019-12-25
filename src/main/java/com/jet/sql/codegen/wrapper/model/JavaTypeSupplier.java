634ff3ba0926968494adb5a5816a2192a649bf85
jetsasank <gorthitk@gmail.com>
2019-12-25 00:10:45 -0500

    Supporting other JDBC Types

--- src/main/java/com/jet/sql/codegen/wrapper/model/JavaTypeSupplier.java
+++ src/main/java/com/jet/sql/codegen/wrapper/model/JavaTypeSupplier.java
@@ -14,14 +14,33 @@ public class JavaTypeSupplier
     {
         switch (jdbcType)
         {
+            case BIT:
+            case BOOLEAN:
+                return "java.lang.Boolean";
+            case TINYINT:
+            case SMALLINT:
+                return "java.lang.Short";
+            case BIGINT:
+                return "java.math.BigInteger";
+            case FLOAT:
+                return "java.lang.Float";
+            case DECIMAL:
+                return "java.lang.Double";
+            case CHAR:
+                return "java.lang.Character";
             case INTEGER:
                 return "java.lang.Integer";
             case VARCHAR:
+            case LONGVARCHAR:
                 return "java.lang.String";
             case ARRAY:
                 return "java.lang.Object[]";
-            case BOOLEAN:
-                return "java.lang.Boolean";
+            case TIME:
+            case TIME_WITH_TIMEZONE:
+                return "java.sql.Time";
+            case TIMESTAMP:
+            case TIMESTAMP_WITH_TIMEZONE:
+                return "java.sql.Timestamp";
             default:
                 return "java.lang.Object";
         }
